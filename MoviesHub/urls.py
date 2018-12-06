705cf7c81972c5af995c5001fe3e1354c37ff74b
dheeraj <dheeraj.agarwal@monoxor.com>
2018-12-07 03:07:15 +0530

    All Data related to movie retrived with the help of threat

--- MoviesHub/urls.py
+++ MoviesHub/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'MoviesHub'
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('movie_details/<int:movie_id>', views.get_movie_details, name='movie_details'),
 ]
--- MoviesHub/views.py
+++ MoviesHub/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 import requests
-
+from threading import Thread
 from Spoon.settings import TMDB_KEY
-
+import queue
+import time
 
 # Create your views here.
 def index(request):
@@ -44,4 +45,57 @@ def index(request):
 
 
 def get_movie_details(request, movie_id):
-    pass
+    if movie_id:
+        primary_info_link = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'?api_key='+TMDB_KEY
+        alternative_link = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'/alternative_titles?api_key='+TMDB_KEY
+        cast_link = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'/credits?api_key='+TMDB_KEY
+        images_link = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'/images?api_key='+TMDB_KEY
+        plot_keywords_link = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'/keywords?api_key='+TMDB_KEY
+        release_info_link = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'/release_dates?api_key='+TMDB_KEY
+        trailers_link = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'/videos?api_key='+TMDB_KEY
+        reviews_link = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'/reviews?api_key='+TMDB_KEY
+        # Taking too much time (calling multiple API) Using threads
+        # Queue is used to store all data
+        data = queue.Queue()
+        primary_info_data = GetData(primary_info_link,data,'primary_info_data')
+        alternative_data = GetData(alternative_link,data,'alternative_data')
+        cast_data = GetData(cast_link,data,'cast_data')
+        images_data = GetData(images_link,data,'images_data')
+        plot_keywords_data = GetData(plot_keywords_link,data,'plot_keywords_data')
+        release_info_data = GetData(release_info_link,data,'release_info_data')
+        trailers_data = GetData(trailers_link,data,'trailers_data')
+        reviews_data = GetData(reviews_link,data,'reviews_data')
+        primary_info_data.start()
+        alternative_data.start()
+        cast_data.start()
+        images_data.start()
+        plot_keywords_data.start()
+        release_info_data.start()
+        trailers_data.start()
+        reviews_data.start()
+        real_data = {}
+        for d in range(8):
+            temp = data.get()
+            real_data[temp[0]] = temp[1]
+        # Data Format
+        return render(request, 'MoviesHub/movie_details.html', {'real_data':real_data})
+    else:
+        redirect('MoviesHub:index')
+
+class GetData(Thread):
+    def __init__(self, link,q, field):
+        super(GetData, self).__init__()
+        self.link = link
+        self.q = q
+        self.field = field
+
+    def run(self):
+        response = requests.get(self.link)
+        if response.status_code == 200:
+            temp = []
+            temp.append(self.field)
+            temp.append(response.json())
+            self.q.put(temp)
+        else:
+            print('Error!', self.link)
+            return {}
